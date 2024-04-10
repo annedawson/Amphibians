08d6371d3564c1ec65c7ae248d8c6315ed8174f1
annedawson <anne.dawson@gmail.com>
2024-04-10 14:50:52 -0700

    Updated AndroidManifest.xml, the Theme files and added the AmphibiansApplication class. 10/4/24

---
+++ app/src/main/java/net/annedawson/amphibians/AmphibiansApplication.kt
@@ -0,0 +1,18 @@
+package net.annedawson.amphibians
+
+import android.app.Application
+
+class AmphibiansApplication : Application(){
+
+    // The Application() class is used to initialize
+    // and provide a dependency injection container
+    // to the rest of the app.
+    // The Application class is the base class
+    // for all Android applications.
+    // It is used to initialize the application
+    // and to provide global resources to the rest of the app.
+
+    // NOTE: AmphibiansApplication is the
+    // Application name in the manifest file
+
+}
--- app/src/main/java/net/annedawson/amphibians/ui/theme/Theme.kt
+++ app/src/main/java/net/annedawson/amphibians/ui/theme/Theme.kt
@@ -58,13 +58,16 @@ fun AmphibiansTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = darkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
